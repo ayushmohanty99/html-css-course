1cc8d7e4d47c13d8cb4bdf5284cfa757b9ac7814
ayushmohanty99 <ayushm.dev@protonmail.com>
2025-01-29 20:11:57 -0500

    Adding Smooth Scrolling and Sticky Nav

--- starter/09-Omnifood-Optimizations/js/script.js
+++ starter/09-Omnifood-Optimizations/js/script.js
@@ -28,6 +28,50 @@ btnNav.addEventListener("click", () => {
   header.classList.toggle('nav-open');
 })
 
+// Smooth Scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    // Scroll to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    // Scroll to other links
+    if (href != "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl)
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if(link.classList.contains('main-nav-link')) {
+      header.classList.toggle('nav-open')
+    }
+  })
+})
+
+// Sticky Navigation 
+const sectionHeroEl = document.querySelector('.section-hero')
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0]; 
+  console.log(ent);
+  if (!ent.isIntersecting) {
+    document.querySelector('body').classList.add('sticky');
+  }
+  else {
+    document.querySelector('body').classList.remove('sticky')
+  }
+}, 
+{
+  // In the viewport
+  root: null, 
+  threshold: 0,
+  rootMargin: "-80px",
+}); 
+obs.observe(sectionHeroEl)
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
